b861669e26c26db54439b9b3091b9610e4100468
Futa Hirakoba <futa-hirakoba@cybozu.co.jp>
2021-02-28 16:51:10 +0900

    chore: refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,28 +15,61 @@ const toBoolean = (value: string) => {
   return undefined;
 };
 
+type Params = {
+  dryRun: boolean;
+  zennMetadata: Partial<ZennMetadata>;
+  githubToken: string;
+  commitSha: string;
+  isForcePush: boolean;
+};
+
+function getParams() {
+  const inputCommitSha = getInput("commit-sha");
+  const title = getInput("title");
+  const emoji = getInput("emoji");
+  const type = getInput("type");
+  const published = getInput("published");
+  const githubToken = getInput("github-token");
+
+  const dryRun = toBoolean(getInput("dry-run"));
+  if (!dryRun) {
+    throw new Error("dryRun is invalid");
+  }
+
+  const commitSha =
+    inputCommitSha === "" ? process.env.GITHUB_SHA : inputCommitSha;
+  if (!commitSha) {
+    throw new Error("commit-sha is invalid");
+  }
+
+  const isForcePush = toBoolean(getInput("force-push"));
+  if (!isForcePush) {
+    throw new Error("force-push is invalid");
+  }
+
+  const zennMetadata: Partial<ZennMetadata> = {
+    title: title === "" ? undefined : title,
+    emoji: emoji === "" ? undefined : emoji,
+    type: type === "" ? undefined : (type as "idea" | "tech"),
+    published: toBoolean(published),
+  };
+
+  const params: Readonly<Params> = {
+    dryRun,
+    zennMetadata,
+    githubToken,
+    commitSha,
+    isForcePush,
+  };
+  return params;
+}
+
 async function run(): Promise<void> {
   try {
-    const dryRun = toBoolean(getInput("dry-run"));
-    const inputCommitSha = getInput("commit-sha");
-    const title = getInput("title");
-    const emoji = getInput("emoji");
-    const type = getInput("type");
-    const published = getInput("published");
-    const githubToken = getInput("github-token");
-
-    const commitSha =
-      inputCommitSha === "" ? process.env.GITHUB_SHA : inputCommitSha;
-    if (!commitSha) {
-      throw new Error("commit-sha is invalid");
-    }
-    const isForcePush = toBoolean(getInput("force-push"));
-    if (!isForcePush) {
-      throw new Error("force-push is invalid");
-    }
+    const params = getParams();
 
     // 変更されたマークダウンの取得とパラメータのアップデート
-    const changedFiles = await getChangedFiles(commitSha);
+    const changedFiles = await getChangedFiles(params.commitSha);
     debug(`changedFiles: ${changedFiles.toString()}`);
     const changedMarkdowns = await getMarkdowns(changedFiles);
     if (changedMarkdowns.length === 0) {
@@ -46,30 +79,29 @@ async function run(): Promise<void> {
     info(`changedMarkdown: ${changedMarkdowns.toString()}`);
 
     // マークダウンの保存とプッシュとプルリクエスト作成
-    const zennMetaData: Partial<ZennMetadata> = {
-      title: title === "" ? undefined : title,
-      emoji: emoji === "" ? undefined : emoji,
-      type: type === "" ? undefined : (type as "idea" | "tech"),
-      published: toBoolean(published),
-    };
     const savedPaths = await saveUpdatedMarkdown(
-      zennMetaData,
+      params.zennMetadata,
       changedMarkdowns
     );
 
     // dry-run = true の場合はプッシュ、プルリクエストの作成をスキップする
-    if (dryRun) {
+    if (params.dryRun) {
       info("dry-run is true. skip after process.");
       return;
     }
 
+    // 変更されたファイルごとにプッシュし、プルリクエストを作成する
     for (const savedPath of savedPaths) {
-      const branchName = await pushChange(savedPath, commitSha, isForcePush);
+      const branchName = await pushChange(
+        savedPath,
+        params.commitSha,
+        params.isForcePush
+      );
       const workflowBranch = process.env.GITHUB_HEAD_REF;
       if (!workflowBranch) {
         throw new Error("GITHUB_HEAD_REF is undefined");
       }
-      const octokit = getOctokit(githubToken);
+      const octokit = getOctokit(params.githubToken);
       await createPullRequest(
         octokit,
         context.repo,
